15d26da9d50801630cdef85749e1e9bf7190b1e0
sham_ibs <sham_ibs@anasystems.co.jp>
2017-05-24 14:51:18 +0900

    Added the logic to determine Win for a particualr request and added comments.

--- src/server/api/play.js
+++ src/server/api/play.js
@@ -9,16 +9,50 @@ export default function(app) {
       array.push(getRandomIntInclusive());
     }
 
+    //get the result of the game.
+    const result = getResult(array);
+
+    //getting a random number for determining bonus.
+    const random = getRandomIntInclusive();
+
+    //setting bonus as true if the random number is 2
+    const bonus = random === 2 ? true : false;
+
     //sending the response as 3 element array
     res.status(200).json([
-      "result",array
+      {"values":array},
+      {"bonus":bonus},
+      {"result":result}
     ]);
   });
 
-  //calculating the random number between 0 and 5
+  //setting the min and max values for getRandomIntInclusive method
   const min = Math.ceil(MIN);
   const max = Math.floor(MAX);
+
+  //Random number generator within a range of 0 and 5
   var getRandomIntInclusive =  () => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  /**
+  Calculating the result of the game
+  All Equal - Big Win
+  Two Equals - Small Win
+  No Equals - No Win
+  */
+  var getResult = (array) => {
+    if (array.length === 3) {
+      if (array[0] === array[1] &&
+          array[0] === array[2]) {
+        return "Big Win"
+      } else if (array[0] === array[1] ||
+                 array[0] === array[2] ||
+                 array[1] === array[2]) {
+        return "Small Win";
+      } else {
+        return "No Win";
+      }
+    }
   }
 }
