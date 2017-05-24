d1495fa78b853802b741e97b413959daa371ab3b
sham_ibs <sham_ibs@anasystems.co.jp>
2017-05-24 11:58:05 +0900

    Added the random number generation method

--- src/server/api/play.js
+++ src/server/api/play.js
@@ -1,7 +1,24 @@
 export default function(app) {
+  const MIN = 0;
+  const MAX = 5;
   app.get('/play', (req, res) => {
+
+    let array = [];
+    //populating the array with 3 elements
+    for (let i = 0; i < 3; i++) {
+      array.push(getRandomIntInclusive());
+    }
+
+    //sending the response as 3 element array
     res.status(200).json([
-      "Hello","Play"
+      "result",array
     ]);
   });
+
+  //calculating the random number between 0 and 5
+  const min = Math.ceil(MIN);
+  const max = Math.floor(MAX);
+  var getRandomIntInclusive =  () => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
