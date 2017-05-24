af1db006c0bda23bba1854dc209b61475b19a595
sham_ibs <sham_ibs@anasystems.co.jp>
2017-05-24 20:40:54 +0900

    Converted util methods to ES6

--- src/server/api/util.js
+++ src/server/api/util.js
@@ -5,40 +5,38 @@ const MAX = 5
 // setting the min and max values for getRandomIntInclusive method
 const min = Math.ceil(MIN)
 const max = Math.floor(MAX)
-module.exports = {
 
-  // Random number generator within a range of 0 and 5
-  getRandomIntInclusive () {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-  },
+// Random number generator within a range of 0 and 5
+export function getRandomIntInclusive () {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
-  /**
-  Calculating the result of the game
-  All Equal - Big Win
-  Two Equals - Small Win
-  No Equals - No Win
-  */
-  getGameResult (array) {
-    if (array.length === 3) {
-      if (array[0] === array[1] &&
-          array[0] === array[2]) {
-        return 'Big Win'
-      } else if (array[0] === array[1] ||
-                 array[0] === array[2] ||
-                 array[1] === array[2]) {
-        return 'Small Win'
-      } else {
-        return 'No Win'
-      }
+/**
+Calculating the result of the game
+All Equal - Big Win
+Two Equals - Small Win
+No Equals - No Win
+*/
+export function getGameResult (array) {
+  if (array.length === 3) {
+    if (array[0] === array[1] &&
+        array[0] === array[2]) {
+      return 'Big Win'
+    } else if (array[0] === array[1] ||
+               array[0] === array[2] ||
+               array[1] === array[2]) {
+      return 'Small Win'
+    } else {
+      return 'No Win'
     }
-  },
-
-  // populating the array with 3 elements
-  populateResultArray () {
-    var array = []
-    for (let i = 0; i < 3; i++) {
-      array.push(this.getRandomIntInclusive())
-    }
-    return array
   }
+}
+
+// populating the array with 3 elements
+export function populateResultArray () {
+  var array = []
+  for (let i = 0; i < 3; i++) {
+    array.push(this.getRandomIntInclusive())
+  }
+  return array
 }
